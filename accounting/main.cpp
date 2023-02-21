01394f88bc34f77d3381f86b79820ae74bda6441
Dmitrii <dtihonov982@gmail.com>
2023-02-21 21:06:44 +0300

    Update main.cpp

--- accounting/main.cpp
+++ accounting/main.cpp
@@ -121,6 +121,29 @@ public:
 
 };
 
+//https://stackoverflow.com/questions/1120140/how-can-i-read-and-parse-csv-files-in-c
+std::vector<std::string> getNextLineAndSplitIntoTokens(std::istream& str)
+{
+    std::vector<std::string>   result;
+    std::string                line;
+    std::getline(str,line);
+
+    std::stringstream          lineStream(line);
+    std::string                cell;
+
+    while(std::getline(lineStream,cell, ','))
+    {
+        result.push_back(cell);
+    }
+    // This checks for a trailing comma with no data after it.
+    if (!lineStream && cell.empty())
+    {
+        // If there was a trailing comma then add an empty element.
+        result.push_back("");
+    }
+    return result;
+}
+
 
 
 int main() {
