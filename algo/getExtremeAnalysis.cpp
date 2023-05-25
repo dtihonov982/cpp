9afc52bcefd5f41631ea288b175b66961e439767
Dmitrii <dtihonov982@gmail.com>
2023-05-25 08:37:48 +0300

    Update getExtremeAnalysis.cpp
    Add comments

--- algo/getExtremeAnalysis.cpp
+++ algo/getExtremeAnalysis.cpp
@@ -1,3 +1,8 @@
+//Analysis which algorithm of getting extreme much faster.
+//1. Simple https://github.com/ClickHouse/ClickHouse/blob/master/src/Columns/ColumnDecimal.cpp#LL399C26-L399C26
+//2. Minimum compare Introduction to Algorithms, 3rd Edition  by Thomas H. Cormen...
+
+
 #include <iostream>
 #include <vector>
 #include <random>
@@ -7,6 +12,82 @@
 #include <cassert>
 #include <sstream>
 #include <variant>
+
+
+//1. Simple https://github.com/ClickHouse/ClickHouse/blob/master/src/Columns/ColumnDecimal.cpp#LL399C26-L399C26
+template <typename T>
+void getExtreme_origin(const std::vector<T>& data, T& min, T& max) {
+    if (data.empty()) {
+        min = T{};
+        max = T{};
+        return;
+    }
+
+    min = data[0];
+    max = data[0];
+    
+
+    for (const auto& x: data) {
+        if (x < min)
+            min = x;
+        else if (max < x)
+            max = x;
+    }
+
+    
+    #if 0   
+    auto it = data.cbegin();
+    const auto end = data.cend(); 
+    for (; it != end; ++it) {
+        const auto it_v = *it;
+        if (it_v < min)
+            min = it_v;
+        else if (it_v > max)
+            max = it_v;
+    } 
+    #endif   
+}
+
+//2. Minimum compare Introduction to Algorithms, 3rd Edition  by Thomas H. Cormen...
+template <typename T>
+void getExtreme_fix(const std::vector<T>& data, T& min, T& max) {
+    if (data.empty()) {
+        min = T{};
+        max = T{};
+        return;
+    }
+
+    min = data[0];
+    max = data[0];
+    
+    //if size not even start with second element
+    auto first = data.begin() + (data.size() & 1);
+    auto second = first + 1;
+    auto end = data.end();
+    while (first < end) {
+        const auto first_v = *first;
+        const auto second_v = *second;
+ 
+        //getting pairs of elements and comparing them       
+        if (first_v < second_v) {
+            //then comparing with min and max
+            if (first_v < min)
+                min = first_v;
+            if (max < second_v)
+                max = second_v;
+        }
+        else {
+            if (second_v < min)
+                min = second_v;
+            if (max < first_v)
+                max = first_v;
+        }
+        
+        first += 2;
+        second = first + 1;
+    }
+
+}
 
 std::vector<int> getRandVector(int size) {
     std::random_device dev;
@@ -21,6 +102,7 @@ std::vector<int> getRandVector(int size) {
     return res;
 }
 
+//Like std::generate
 template <typename GenType>
 auto getRandVector(int size, GenType& gen) -> std::vector<decltype(gen())> {
     std::vector<decltype(gen())> result;
@@ -31,6 +113,9 @@ auto getRandVector(int size, GenType& gen) -> std::vector<decltype(gen())> {
     return result;
 }
 
+//Generators for benchmarking
+
+//Generator of random integer in [min; max]
 class IntGen {
 public:
 
@@ -48,6 +133,7 @@ protected:
     std::uniform_int_distribution<std::mt19937::result_type> valueDist;
 };
 
+//Generator of random string with fixed size or size in [minSize; maxSize]
 class StrGen {
 public:
     
@@ -83,89 +169,7 @@ private:
     const bool randSize = true;
 };
 
-std::string getRandStr(int size) {
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(33, 126); 
-    
-    std::stringstream ss;
-    for (int i = 0; i < size; ++i) {
-        ss << static_cast<char>(dist(rng));
-    }
-    
-    return ss.str();
-}    
-    
-template <typename T>
-void getExtreme_origin(const std::vector<T>& data, T& min, T& max) {
-    if (data.empty()) {
-        min = T{};
-        max = T{};
-        return;
-    }
-
-    min = data[0];
-    max = data[0];
-    
-    #if 0
-    for (const auto& x: data) {
-        if (x < min)
-            min = x;
-        else if (max < x)
-            max = x;
-    }
-    #endif
-    
-    auto it = data.cbegin();
-    const auto end = data.cend(); 
-    for (; it != end; ++it) {
-        const auto it_v = *it;
-        if (it_v < min)
-            min = it_v;
-        else if (it_v > max)
-            max = it_v;
-    }    
-}
-
-template <typename T>
-void getExtreme_fix(const std::vector<T>& data, T& min, T& max) {
-    if (data.empty()) {
-        min = T{};
-        max = T{};
-        return;
-    }
-
-    min = data[0];
-    max = data[0];
-    
-    #if 1
-    auto first = data.begin() + (data.size() & 1);
-    auto second = first + 1;
-    auto end = data.end();
-    while (first < end) {
-        const auto first_v = *first;
-        const auto second_v = *second;
-        
-        if (first_v < second_v) {
-            if (first_v < min)
-                min = first_v;
-            if (max < second_v)
-                max = second_v;
-        }
-        else {
-            if (second_v < min)
-                min = second_v;
-            if (max < first_v)
-                max = first_v;
-        }
-        
-        first += 2;
-        second = first + 1;
-    }
-    #endif
-    
-
-}
+//Tests
 
 bool test_int(int size) {
     int min = -1;
@@ -198,24 +202,7 @@ bool test_str(int size) {
 template <typename T>
 using TargetFuncType = void (*)(const std::vector<T>&, T& , T&);
 
-template <typename Func, Func func>
-std::chrono::milliseconds measure_(int size) {
-    std::vector<int> vec = getRandVector(size);
-    int min = -1;
-    int max = -1;    
-    
-    auto start = std::chrono::high_resolution_clock::now();
-    func(vec, min, max);
-    auto finish = std::chrono::high_resolution_clock::now();
-    
-    if (size > 0)
-        assert( *std::min_element(vec.begin(), vec.end()) == min &&
-               *std::max_element(vec.begin(), vec.end()) == max );
-    else 
-        assert( !min && !max ); 
-    return std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
-}
-
+//Returns time of executing function Target with type Func and arguments args
 template <typename Func, Func target, typename... Types>
 std::chrono::milliseconds measure(Types&&... args) {
     auto start = std::chrono::high_resolution_clock::now();
@@ -224,6 +211,7 @@ std::chrono::milliseconds measure(Types&&... args) {
     return std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 }
 
+//
 template<typename GenType>
 void bench(GenType& gen) {
     using ValueType = decltype(gen());
@@ -239,16 +227,6 @@ void bench(GenType& gen) {
     }
 }
 
-void test_all() {
-    bool passed = false;
-    for (int i = 0; i < 1'000; ++i) {
-        passed = test_int(i);
-        if (!passed)
-            break;
-    }   
-    std::cout << std::boolalpha << passed << std::endl;
-}
-
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     for (const auto& x: vec) {
@@ -261,10 +239,9 @@ int main()
 {
     std::cout << std::boolalpha << test_str(10) << std::endl;
     std::cout << std::boolalpha << test_int(10) << std::endl;
+
     auto gen = IntGen{-1000, 1000};
     bench(gen);
-    
-    std::cout << IntGen{-1000, 1000}() << std::endl;
     
     return 0;
 }
