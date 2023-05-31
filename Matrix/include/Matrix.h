067d1b067acb5239d30455202f0c85bd2ba20152
Dmitrii <dtihonov982@gmail.com>
2023-05-31 11:26:02 +0300

    Update Matrix.h

--- Matrix/include/Matrix.h
+++ Matrix/include/Matrix.h
@@ -3,31 +3,17 @@
 
 #include <iostream>
 #include <cstring>
+#include <cassert>
+#include <iterator>
 
 template<typename T>
 class Matrix {
 public:
     Matrix();
-    Matrix(int m_, int n_);
+    Matrix(int rows_, int cols_);
+    Matrix(int rows_, int cols_, const T& value);
     Matrix(std::initializer_list<std::initializer_list<T>> lst); 
     Matrix(std::istream& is);
-    
-    class Row {
-    private:
-        T* row;
-    public:
-        Row(T* row_): row(row_) {}
-        const T& operator[](int i) const { return row[i]; }
-        T& operator[](int i) { return row[i]; }
-    };
-
-    
-    class iterator {
-        T* ptr;
-    public:
-        T& operator++();
-        T operator++(int);
-    };
 
     Matrix(const Matrix<T>& other);
     Matrix<T>& operator=(const Matrix<T>& other);
@@ -35,164 +21,187 @@ public:
     Matrix<T>& operator=(Matrix<T>&& other) noexcept;
     ~Matrix();
     
-    void output(std::ostream& out) const;
+    void fill(const T& value);
+
+    struct RowProxy {
+        T* row;
+        const T& operator[](int i) const { return row[i]; }
+        T& operator[](int i) { return row[i]; }
+    };   
+    RowProxy operator[](int i) { return RowProxy{data + i * cols}; }
+    const RowProxy operator[](int i) const { return RowProxy{data + i * cols}; }
+    T& get(int i, int j) { return data[i * cols + j]; }
+    const T& get(int i, int j) const { return data[i * cols + j]; }
+
+    void dump(std::ostream& out) const;
+    Matrix clone() const;
     
-    void free();
-    void copy(const Matrix<T>& rhs);
-
-    Row operator[](int i) { return Row(data + i * cols); }
-    const Row operator[](int i) const { return Row(data + i * cols); }
     Matrix<T> product(const T& x) const; 
-
-    T& at(int i, int j);
     Matrix<T> product(const Matrix<T>& B) const;
     Matrix<T> pow(int n) const;
     Matrix<T> minorMatrix(int exRow, int exCol) const;
     T det() const;
+    
+    static bool same_shapes(const Matrix& lhs, const Matrix& rhs);
+    
 private:
+    void allocate();
+    void free();
+    void copy(const Matrix<T>& rhs);
+    
     T* data = nullptr;
     int rows = 0;
     int cols = 0;
 };
 
 template<typename T>
+Matrix<T> Matrix<T>::clone() const {
+    return Matrix{*this}; 
+}
+
+template<typename T>
+void Matrix<T>::allocate() {
+    data = new T[rows * cols];
+}
+
+template<typename T>
+void Matrix<T>::free() {
+    delete[] data;
+}
+
+template<typename T>
+void Matrix<T>::fill(const T& value) {
+    std::fill(data, data + rows * cols, value);
+}
+
+template<typename T>
 Matrix<T>::Matrix() {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix default ctor. " << this << std::endl;
 #endif
 }
 
 template<typename T>
 Matrix<T>::Matrix(int rows_, int cols_) {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix(n, m) ctor. " << this << std::endl;
 #endif
     rows = rows_;
     cols = cols_;
-    data = new T[rows * cols];
+    allocate();
+}
+
+template<typename T>
+Matrix<T>::Matrix(int rows_, int cols_, const T& value)
+: Matrix(rows_, cols_) {
+#ifndef NDEBUG
+    std::cout << "# Matrix(n, m, value) ctor. " << this << std::endl;
+#endif
+    fill(value);
 }
 
 template<typename T>
 Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> lst) {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix init_lst ctor. " << this << std::endl;
 #endif
     rows = lst.size();
     cols = lst.begin()->size();
-    data = new T[rows * cols];
-    int i = 0;
-    for (auto it = lst.begin(), et = lst.end(); it != et; ++it) {
-        for (auto jt = it->begin(), jte = it->end(); jt != jte; ++jt) {
-           data[i++] = *jt; 
-        }
+    allocate();
+    
+    T* ptr = data;
+    for (const auto& row: lst) {
+        std::copy(row.begin(), row.end(), ptr);
+        ptr += cols;
     }
 }
 
 template<typename T>
 Matrix<T>::Matrix(std::istream& is) {
-    is >> rows;
-    is >> cols;
-    data = new T[rows * cols];
-    for (int i = 0; i < rows * cols; ++i) {
-        is >> data[i];
-    }
-}
-
-template<typename T>
-Matrix<T>::Matrix(const Matrix<T>& other) {
-#ifdef DEBUG
-    std::cout << "# Matrix copy ctor. " << this << std::endl;
+#ifndef NDEBUG
+    std::cout << "# Matrix istream ctor. " << this << std::endl;
 #endif
-    copy(other);
+    std::istream_iterator<T> it{is}, end;
+    rows = *it++;
+    cols = *it++;
+    allocate();
+    std::copy(it, end, data);
 }
 
 template<typename T>
 void Matrix<T>::copy(const Matrix<T>& other) {
     rows = other.rows;
     cols = other.cols;
-    data = new T[rows * cols];
+    allocate();
     std::copy(other.data, other.data + other.rows * other.cols, data);
 }
 
 template<typename T>
+Matrix<T>::Matrix(const Matrix<T>& other) {
+#ifndef NDEBUG
+    std::cout << "# Matrix copy ctor. " << this << std::endl;
+#endif
+    copy(other);
+}
+
+template<typename T>
 Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other) {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix copy assignment. " << this << std::endl;
 #endif
     if (this == &other) {
         return *this;
     }
-    delete[] data;
+    free();
     copy(other);
     return *this;
 }
 
 template<typename T>
 Matrix<T>::Matrix(Matrix<T>&& other) noexcept {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix move ctor. " << this << std::endl;
 #endif
     data = other.data;
     rows = other.rows;
     cols = other.cols;
     other.data = nullptr;
-    other.rows = 0;
-    other.cols = 0;
 }
 
 template<typename T>
 Matrix<T>& Matrix<T>::operator=(Matrix<T>&& other) noexcept {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix move assign. " << this << std::endl;
 #endif
     if (this == &other) {
         return *this;
     }
-    delete[] data;
+    free();
     data = other.data;
     rows = other.rows;
     cols = other.cols;
     other.data = nullptr;
-    other.rows = 0;
-    other.cols = 0;
     return *this;
 }
 
 template<typename T>
 Matrix<T>::~Matrix() {
-#ifdef DEBUG
+#ifndef NDEBUG
     std::cout << "# Matrix dtor  " << this << " data=" << data << std::endl;
 #endif
-    delete[] data;    
+    free();    
 }
 
-#if 0
 template<typename T>
-void Matrix<T>::free() {
-    for (int i = 0; i < rows; ++i) {
-        delete[] data[i];
-    }
-    delete[] data;
-}
-#endif
-
-template<typename T>
-void Matrix<T>::output(std::ostream& out) const {
+void Matrix<T>::dump(std::ostream& out) const {
     out << rows << ' ' << cols << std::endl;
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            out << data[cols * i + j] << " ";
+            out << get(i, j) << " ";
         }
         out << std::endl;
     }
 }
-
-#if 0 
-template<typename T>
-T* Matrix<T>::operator[](int i) {
-    return data + i * rows;
-}
-#endif
 
 template<typename T>
 Matrix<T> Matrix<T>::product(const Matrix& B) const {
@@ -228,12 +237,6 @@ Matrix<T> Matrix<T>::pow(int n) const {
 }
 
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const Matrix<T>& M) {
-    M.output(os);
-    return os;
-}
-
-template<typename T>
 Matrix<T> Matrix<T>::product(const T& x) const {
     Matrix<T> result{*this};
     for (int i = 0; i < rows; i++) {
@@ -253,17 +256,6 @@ template<typename T>
 Matrix<T> operator*(const Matrix<T>& matrix, const T& number) {
     return matrix.product(number);
 }
-
-/*
-template<typename T>
-void fillRand(int start, end int) {
-    std::srand(std::time(NULL));
-    for (int i = 0; i < 
-
-template<typename T>
-T Matrix<T>::det() const {
-}
-*/
 
 template<typename T>
 Matrix<T> Matrix<T>::minorMatrix(int exRow, int exCol) const {
@@ -299,11 +291,5 @@ T Matrix<T>::det() const {
     }
     return result;
 }
-
-
-
-
-
-
 
 #endif
