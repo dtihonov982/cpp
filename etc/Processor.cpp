57cd31e74f59de78657efdaa421bd9ad73cdfd92
Dmitrii <dtihonov982@gmail.com>
2023-06-29 13:07:09 +0300

    Update Processor.cpp
    Add stack

--- etc/Processor.cpp
+++ etc/Processor.cpp
@@ -1,3 +1,4 @@
+//TODO: JMP_I
 #include <iostream>
 #include <array>
 #include <algorithm>
@@ -10,7 +11,7 @@
 
 #include <cassert>
 
-#define MEM_SIZE 32
+#define MEM_SIZE 64
 #define OPCODE_WC 3
 #define TIME_LIMIT 1000
 #define FLAGS_COUNT 8
@@ -22,27 +23,31 @@ using Opcode    = std::array<Word, OPCODE_WC>;
 
 namespace reg {
 
-enum RegId { r0 = 0, r1, r2, r3, rip = 100 };
+enum RegId { r0 = 0, r1, r2, r3, rip = 10, rsp = 15, rbp};
 enum FlagId { ZF = 6 };
 
 class Registers {
 public:
     Registers() {
-        regs_ = {{reg::r0, 0}, {reg::r1, 0}, {reg::r2, 0}, {reg::r3, 0}, {reg::rip, 0}};
+        regs_ = {{r0, 0}, {r1, 0}
+               , {r2, 0}, {r3, 0}
+               , {rip, 0}
+               , {rsp, 0}, {rbp, 0}
+            };
     }
-    Word getReg(RegId id) {
+    Word get(RegId id) {
         return regs_[id];
     }
-    void setReg(RegId id, Word data) {
+    void set(RegId id, Word data) {
         regs_[id] = data;
     }
-    bool getFlag(FlagId id) {
+    bool get(FlagId id) {
         return flags_[id];
     }
-    void setFlag(FlagId id) {
+    void set(FlagId id) {
         flags_[id] = true;
     }
-    void resetFlag(FlagId id) {
+    void reset(FlagId id) {
         flags_[id] = false;
     }
 
@@ -105,6 +110,10 @@ namespace cmd {
     struct End;
     struct Jmp;
     struct Jz;
+    struct MovRI;
+    struct MovRR;
+    struct PushR;
+    struct PopR;
 }
 
 class ICommandVisitor {
@@ -112,9 +121,13 @@ public:
     virtual void visit(cmd::End& cm)   = 0;
     virtual void visit(cmd::AddRR& cm) = 0;
     virtual void visit(cmd::AddRI& cm) = 0;
+    virtual void visit(cmd::MovRI& cm) = 0;
+    virtual void visit(cmd::MovRR& cm) = 0;
     virtual void visit(cmd::CmpRI& cm)   = 0;
     virtual void visit(cmd::Jmp& cm)   = 0;
     virtual void visit(cmd::Jz& cm)   = 0;
+    virtual void visit(cmd::PushR& cm)   = 0;
+    virtual void visit(cmd::PopR& cm)   = 0;
 };
 
 
@@ -123,10 +136,13 @@ namespace cmd {
         END    = 0
       , ADD_RR = 10
       , ADD_RI = 11
+      , MOV_RR = 20
       , MOV_RI = 21
       , CMP_RI = 31
       , JMP    = 50
       , JZ
+      , PUSH_R  = 70
+      , POP_R  
     };
 
     class ICommand {
@@ -174,6 +190,32 @@ namespace cmd {
         }
     };
 
+    struct MovRI: public ICommand {
+        MovRI(reg::RegId dst, Word val)
+        : ICommand(MOV_RI)
+        , dst_reg(dst)
+        , val_(val) {
+        }
+        reg::RegId dst_reg;
+        Word val_;
+        void accept(ICommandVisitor* cv) override {
+            cv->visit(*this);
+        }
+    };
+
+    struct MovRR: public ICommand {
+        MovRR(reg::RegId dst, reg::RegId src)
+        : ICommand(MOV_RR)
+        , dst_reg(dst)
+        , src_reg(src) {
+        }
+        reg::RegId dst_reg;
+        reg::RegId src_reg;
+        void accept(ICommandVisitor* cv) override {
+            cv->visit(*this);
+        }
+    };
+
     struct Jmp: public ICommand {
         Jmp(Address addr)
         : ICommand(JMP)
@@ -209,6 +251,28 @@ namespace cmd {
         }        
     };
 
+    struct PushR: public ICommand {
+        PushR(reg::RegId reg)
+        : ICommand(PUSH_R)
+        , src_reg(reg) {
+        }
+        reg::RegId src_reg;
+        void accept(ICommandVisitor* cv) override {
+            cv->visit(*this);
+        }        
+    };
+
+    struct PopR: public ICommand {
+        PopR(reg::RegId reg)
+        : ICommand(POP_R)
+        , dst_reg(reg) {
+        }
+        reg::RegId dst_reg;
+        void accept(ICommandVisitor* cv) override {
+            cv->visit(*this);
+        }        
+    };
+
     using list = std::vector<cmd::ICommand*>;
 }
 
@@ -233,6 +297,16 @@ public:
         append(opcode);
     }
 
+    void visit(cmd::MovRI& cm) override {
+        Opcode opcode = {cm.code_, cm.dst_reg, cm.val_};
+        append(opcode);
+    }
+
+    void visit(cmd::MovRR& cm) override {
+        Opcode opcode = {cm.code_, cm.dst_reg, cm.src_reg};
+        append(opcode);
+    }
+
     void visit(cmd::Jmp& cm) override {
         Opcode opcode = {cm.code_, cm.addr_, 0};
         append(opcode);
@@ -245,6 +319,16 @@ public:
 
     void visit(cmd::CmpRI& cm) override {
         Opcode opcode = {cm.code_, cm.reg_, cm.val_};
+        append(opcode);
+    }
+
+    void visit(cmd::PushR& cm) override {
+        Opcode opcode = {cm.code_, cm.src_reg, 0};
+        append(opcode);
+    }
+
+    void visit(cmd::PopR& cm) override {
+        Opcode opcode = {cm.code_, cm.dst_reg, 0};
         append(opcode);
     }
 
@@ -309,6 +393,20 @@ public:
         return ICommandPtr{cm};
     }
 
+    static ICommandPtr MovRI_dec(Opcode opc) {
+        reg::RegId dst_reg = static_cast<reg::RegId>(opc[1]);
+        Word val = opc[2];
+        cmd::ICommand* cm = new cmd::MovRI{dst_reg, val};
+        return ICommandPtr{cm};
+    }
+
+    static ICommandPtr MovRR_dec(Opcode opc) {
+        reg::RegId dst_reg = static_cast<reg::RegId>(opc[1]);
+        reg::RegId src_reg = static_cast<reg::RegId>(opc[2]);
+        cmd::ICommand* cm = new cmd::MovRI{dst_reg, src_reg};
+        return ICommandPtr{cm};
+    }
+
     static ICommandPtr CmpRI_dec(Opcode opc) {
         reg::RegId dst_reg = static_cast<reg::RegId>(opc[1]);
         Word  val = static_cast<Word>(opc[2]);
@@ -320,9 +418,24 @@ public:
         cmd::ICommand* cm = new cmd::Jmp{opc[1]};
         return ICommandPtr{cm};
     }
+
     static ICommandPtr Jz_dec(Opcode opc) {
         assert(opc[2] == 0);
         cmd::ICommand* cm = new cmd::Jz{opc[1]};
+        return ICommandPtr{cm};
+    }
+
+    static ICommandPtr PushR_dec(Opcode opc) {
+        assert(opc[2] == 0);
+        reg::RegId src_reg = static_cast<reg::RegId>(opc[1]);
+        cmd::ICommand* cm = new cmd::PushR{src_reg};
+        return ICommandPtr{cm};
+    }
+
+    static ICommandPtr PopR_dec(Opcode opc) {
+        assert(opc[2] == 0);
+        reg::RegId dst_reg = static_cast<reg::RegId>(opc[1]);
+        cmd::ICommand* cm = new cmd::PopR{dst_reg};
         return ICommandPtr{cm};
     }
 private:
@@ -333,36 +446,47 @@ const std::unordered_map<cmd::Code, DecodingHandler> Decoder::decs {
       {cmd::END,          End_dec}
     , {cmd::ADD_RR,       AddRR_dec}
     , {cmd::ADD_RI,       AddRI_dec}
+    , {cmd::MOV_RR,       MovRR_dec}
+    , {cmd::MOV_RI,       MovRI_dec}
     , {cmd::JMP,          Jmp_dec} 
     , {cmd::JZ,           Jz_dec} 
     , {cmd::CMP_RI,       CmpRI_dec} 
+    , {cmd::PUSH_R,       PushR_dec} 
+    , {cmd::POP_R,       PopR_dec} 
 };
 
 } //namespace tr
 
 class Processor: public ICommandVisitor {
 public:
+    
     Processor(Memory& mem, reg::Registers& regs)
     : mem_(mem)
     , regs_(regs) {
     }
 
     void run() {
-        rip = 0;
+        initStack();
         for (int t = 0; isActive_ && t < TIME_LIMIT; ++t) {
-            Word rip_before = rip;
+            Word rip_before = regs_.get(reg::rip);
             Opcode opc = getNextInstruction();
             auto cm = tr::Decoder::decode(opc);
             cm->accept(this);
+            Word rip_after = regs_.get(reg::rip);
             //If command doesn't modifies rip, then go to the next command in memory
-            if (rip_before == rip)
-                rip += OPCODE_WC;
+            if (rip_before == rip_after)
+                regs_.set(reg::rip, rip_before += OPCODE_WC);
         }
+    }
+
+    void initStack() {
+        regs_.set(reg::rsp, MEM_SIZE - 1);
+        regs_.set(reg::rbp, MEM_SIZE - 1);
     }
 
     Opcode getNextInstruction() {
         assert(isActive_);
-        Word instrAddr = getRip();
+        Word instrAddr = regs_.get(reg::rip);
         assert(instrAddr <= mem_.size() - OPCODE_WC);
         Opcode opc;
         for (int i = 0; i < OPCODE_WC; ++i) {
@@ -375,22 +499,32 @@ public:
     void visit(cmd::AddRR& cm) override {
         reg::RegId dst = cm.dst_reg;
         reg::RegId src = cm.src_reg;
-        Word dst_v = regs_.getReg(dst);
-        Word src_v = regs_.getReg(src);
+        Word dst_v = regs_.get(dst);
+        Word src_v = regs_.get(src);
         dst_v += src_v;
-        regs_.setReg(dst, dst_v);
+        regs_.set(dst, dst_v);
         if (dst_v == 0)
-            regs_.setFlag(reg::ZF);
+            regs_.set(reg::ZF);
     }
 
     void visit(cmd::AddRI& cm) override {
         reg::RegId dst = cm.dst_reg;
-        Word dst_v = regs_.getReg(dst);
+        Word dst_v = regs_.get(dst);
         Word src_v = cm.val_;
         dst_v += src_v;
-        regs_.setReg(dst, dst_v);
+        regs_.set(dst, dst_v);
         if (dst_v == 0)
-            regs_.setFlag(reg::ZF);
+            regs_.set(reg::ZF);
+    }
+
+    void visit(cmd::MovRI& cm) override {
+        reg::RegId dst = cm.dst_reg;
+        regs_.set(dst, cm.val_);
+    }
+
+    void visit(cmd::MovRR& cm) override {
+        auto src_v = regs_.get(cm.src_reg);
+        regs_.set(cm.dst_reg, src_v);
     }
 
     void visit(cmd::End& cm) override {
@@ -400,29 +534,42 @@ public:
 
     void visit(cmd::CmpRI& cm) override {
         reg::RegId reg = cm.reg_;
-        Word reg_v = regs_.getReg(reg);
+        Word reg_v = regs_.get(reg);
         Word tmp = reg_v - cm.val_;
         if (tmp == 0)
-            regs_.setFlag(reg::ZF);
+            regs_.set(reg::ZF);
     }
 
     void visit(cmd::Jmp& cm) override {
-        setRip(cm.addr_);
+        regs_.set(reg::rip, cm.addr_);
     }
 
     void visit(cmd::Jz& cm) override {
-        if (regs_.getFlag(reg::ZF))
-           setRip(cm.addr_);
-    }
-    
-    Word getRip() {
-        return rip;
+        if (regs_.get(reg::ZF))
+            regs_.set(reg::rip, cm.addr_);
     }
 
-    void setRip(Address dst_address) {
-        assert(dst_address < mem_.size());
-        rip = dst_address;
+    void visit(cmd::PushR& cm) override {
+        //SUB RSP, 8
+        auto addrInRsp = regs_.get(reg::rsp);
+        addrInRsp--; 
+        regs_.set(reg::rsp, addrInRsp);
+
+        //MOV [RSP], R0
+        auto src_v = regs_.get(cm.src_reg);
+        mem_.set(addrInRsp, src_v); 
     }
+
+    void visit(cmd::PopR& cm) override {
+        //MOV R0, [RSP]
+        auto addrInRsp = regs_.get(reg::rsp);
+        auto valueInStack = mem_.get(addrInRsp);
+        regs_.set(cm.dst_reg, valueInStack);
+        //ADD RSP, 8
+        ++addrInRsp;
+        regs_.set(reg::rsp, addrInRsp);
+    }
+
     void dump() {
         std::cout << "--------------Processor dump ---------------\n";
         regs_.dump();
@@ -437,23 +584,22 @@ private:
 
 int main() {
     using namespace cmd;
-    Address loopLabel = 3;
-    Address doneLabel = 3 + 6*3;
-    AddRI c0{reg::r0, 100};
-    CmpRI c1{reg::r0, 0};
-    Jz    c2{doneLabel};
-    AddRI c3{reg::r3, 1};
-    AddRR c4{reg::r1, reg::r3};
-    AddRI c5{reg::r0, -1};
-    Jmp   c6{loopLabel};
-    cmd::list prog{&c0, &c1, &c2, &c3, &c4, &c5, &c6};
+    using namespace reg;
 
-    tr::Encoder encoder;
-    Block encodedProg = encoder.encode(prog);
+    Block encodedProg = {
+        MOV_RI, r0, 1
+      , MOV_RI, r1, 2
+      , MOV_RI, r2, 3
+      , PUSH_R, r0, 0
+      , PUSH_R, r1, 0
+      , PUSH_R, r2, 0
+      , POP_R,  r0, 0
+      , POP_R,  r1, 0
+      , POP_R,  r2, 0
+    };
 
     Memory mem{encodedProg};
     reg::Registers regs;
-
 
     Processor proc{mem, regs};
     proc.dump();
