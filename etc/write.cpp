0583ec1925126e7f865c2bea63823f9d2888ce56
Dmitrii <dtihonov982@gmail.com>
2023-07-06 16:36:43 +0300

    Update write.cpp

--- etc/write.cpp
+++ etc/write.cpp
@@ -21,32 +21,41 @@ Buffer getBuffer(size_t count) {
 
 using Benchmark = std::vector<int>;
 
-Benchmark writeFile(std::string_view fileName, size_t fileSize, size_t bufferSize) {
+Benchmark writeFile(int fd, size_t fileSize, size_t bufferSize) {
     Benchmark result;
-    int fd = creat(fileName.data(), 0);
+    //int fd = creat(fileName.data(), 0);
     int operCount = fileSize / bufferSize;
-    operCount += (fileSize % bufferSize != 0); //+1 if fileSize not divides on bufferSize
+    Buffer buff = getBuffer(bufferSize);
+    auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < operCount; ++i) {
-        Buffer buff = getBuffer(bufferSize);
-        auto start = std::chrono::high_resolution_clock::now();
+        //auto start = std::chrono::high_resolution_clock::now();
         ssize_t written = write(fd, buff.data(), buff.size());
+#if 0
         auto stop = std::chrono::high_resolution_clock::now();    
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
         result.push_back(duration);
-        //std::cout << written << '\n';
+        std::cout << written << '\n';
         if  (written != bufferSize) {
             std::cout << "Error. written=" << written << '\n';
             break;
         }
+#endif
     }
-    close(fd);
+    auto stop = std::chrono::high_resolution_clock::now();    
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+    std::cout << bufferSize << ';' << duration << '\n';
+    //close(fd);
     return result;
 }
 
 int main() {
-    auto result = writeFile("test.dat", 10 * Mb, 128);
-    std::cout << std::accumulate(result.begin(), result.end(), 0) << '\n';
-    std::cout << result.size() << '\n';
+    int fd = creat("test.dat", 0);
+    for (int i = 32; i < 8*Kb; i <<= 1)
+    //for (int i = 512; i <= 16*Kb; i += 512)
+        writeFile(fd, 10 * Mb, i);
+    close(fd);
+    //std::cout << std::accumulate(result.begin(), result.end(), 0) << '\n';
+    //std::cout << result.size() << '\n';
    // close(res);
 
 }
