45eb73ad208df78a9e6a702a1b1854a975eea571
Dmitrii <dtihonov982@gmail.com>
2023-07-31 09:04:46 +0300

    Update sort.asm

--- vmachine/sort.asm
+++ vmachine/sort.asm
@@ -1,6 +1,6 @@
 section .data
     array  dq  14, 27, 20, 07, 23
-    len    equ ($ - array) / 8
+    size   equ ($ - array) / 8
 section .bss
 section .text
     global main
@@ -9,39 +9,43 @@ main:
     mov     rbp,  rsp
     
     mov     rdi,  array
-    mov     rsi,  len
+    mov     rsi,  size
     call    sort
 
     leave
     ret
 
+; bubble sort. rdi = array, rsi = size
 sort:
     push    rbp
     mov     rbp, rsp
 
-.loop1_init:
+; outer loop for( int i = 1; i < size; ++i) 
+.loop1_init: 
 
-    mov     rcx, 1
+    mov     rcx, 1                ;  i = 1
     jmp     .loop1_cond
 
 .loop1: 
+
+; inner loop for(int j = i; j > 0; ++i) 
 .loop2_init:
     
-    mov     rdx, rcx
+    mov     rdx, rcx              ; j = i
     jmp     .loop2_cond
 
 .loop2:
 
-    lea     r10, [rdi + 8 * rdx] ; r10 = A + j
-    lea     r11, [r10 - 8] ; r11 = A + j - 1
+    lea     r10, [rdi + 8 * rdx]  ; r10 = A + j   
+    lea     r11, [r10 - 8]        ; r11 = A + j - 1
 
-    mov     r12, [r10]
-    mov     r13, [r11]
+    mov     r12, [r10]            ; r12 = *(A + j)
+    mov     r13, [r11]            ; r13 = *(A + j - 1)
 
 .if_cond:
 
-    cmp     r12, r13 ; A[j] cmp A[j-1]
-    jl      .if_branch
+    cmp     r12, r13              
+    jl      .if_branch            ; if (A[j] < A[j-1])
 
 .else_branch:
     
@@ -51,29 +55,29 @@ sort:
 .if_branch:
 
     mov     [r10], r13
-    mov     [r11], r12 ; swap(A[j], A[j-1])
+    mov     [r11], r12            ; swap(A[j], A[j-1])
 
 .end_if:
 
 .loop2_inc:
     
-    dec     rdx
+    dec     rdx                   ; --j
 
 .loop2_cond:
 
     cmp     rdx, 0
-    jg      .loop2
+    jg      .loop2                ; j > 0
 
 .loop2_done:
 
 .loop1_inc:
 
-    inc     rcx
+    inc     rcx                   ; ++i
 
 .loop1_cond:
 
     cmp     rcx, rsi
-    jl      .loop1
+    jl      .loop1                ; i < size
 
 .loop1_done:
 
